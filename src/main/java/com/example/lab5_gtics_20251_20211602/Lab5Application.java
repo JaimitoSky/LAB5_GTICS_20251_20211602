f8203febe85a3a31e066a6a25537f74192f7dc9c
JaimitoSky <edgar070704@hotmail.com>
2025-05-15 20:31:31 -0500

    Inicio  de LAB5

---
+++ src/main/java/com/example/lab5_gtics_20251_20211602/Lab5Application.java
@@ -0,0 +1,12 @@
+package com.example.lab5_gtics_20251_20211602;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5Application.class, args);
+    }
+}
+
